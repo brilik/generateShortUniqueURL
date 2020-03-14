e561978fba9e412dedd007393b4cb0ec30af774c
brilik <megabrilik@gmail.com>
2020-03-14 04:28:01 +0200

    added initial result generate url

--- front/themes/assets/js/custom.js
+++ front/themes/assets/js/custom.js
@@ -1,49 +1,61 @@
-document.addEventListener("DOMContentLoaded", load);
+document.addEventListener("DOMContentLoaded", load)
 
 
 function load() {
-    initResultGenerateURL('generate');
+    initResultGenerateURL('generate')
 }
 
 
-function initResultGenerateURL(formId){
+function initResultGenerateURL(formId) {
     let form = document.getElementById(formId) // search form
     let input = form.querySelector('input[type=url]') // search input url
 
     form.onsubmit = (event) => {
-        event.preventDefault(); // stop send form
+        event.preventDefault() // stop send form
         let content = input.value // get content
-        // content send to server
-        // get result
-        createAndShowBlockResult(form, 'result from php') // create block result and show
+        ajax(form, content) // content send to server and get result
     }
 }
 
 function copyToClipboard() {
-    var textBox = document.querySelector(".unique-url__result input");
-    textBox.select();
-    document.execCommand("copy");
-    console.log('copied');
+    var textBox = document.querySelector(".unique-url__result input")
+    textBox.select()
+    document.execCommand("copy")
+    console.log('copied')
 }
 
-function createAndShowBlockResult(form, data){
+function createAndShowBlockResult(form, data) {
     let result = document.createElement('div')
     let resultForCopy = document.createElement('input')
     let buttonCopyResultToClipboard = document.createElement('button')
     buttonCopyResultToClipboard.className = "btn btn-primary btn-block"
-    buttonCopyResultToClipboard.onclick = () => ( copyToClipboard() )
+    buttonCopyResultToClipboard.onclick = () => (copyToClipboard())
     buttonCopyResultToClipboard.innerText = "Copy"
     resultForCopy.type = "text"
     resultForCopy.classList.add('form-control')
     resultForCopy.setAttribute("value", data)
-    resultForCopy.setAttribute('readonly','')
+    resultForCopy.setAttribute('readonly', '')
     result.className = 'unique-url__result'
     result.appendChild(resultForCopy)
     result.appendChild(buttonCopyResultToClipboard)
 
-    if( document.querySelector(".unique-url__result") ){
+    if (document.querySelector(".unique-url__result")) {
         document.querySelector(".unique-url__result").remove()
     }
 
     form.appendChild(result) // paste result
+}
+
+function ajax(form, data) {
+    let ajaxObject = new XMLHttpRequest()
+
+    ajaxObject.open('POST', 'back/ajax.php') // open connection
+    ajaxObject.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded') // install format
+    ajaxObject.send('url=' + encodeURIComponent(data)) // send POST-request
+    ajaxObject.onreadystatechange = function () {
+        if (ajaxObject.readyState === 4 && ajaxObject.status === 200) { // get answer success and ready from processing
+            createAndShowBlockResult(form, ajaxObject.responseText) // create block result and show
+        } else {
+        }
+    }
 }
