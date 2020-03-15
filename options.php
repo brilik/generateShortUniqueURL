27e58d1a6a380898cf778275ae1d7e1336f06d9b
brilik <megabrilik@gmail.com>
2020-03-15 06:18:20 +0200

    Defines of access for database

--- options.php
+++ options.php
@@ -3,6 +3,10 @@
 define('APP', dirname(__FILE__));
 define('DIR_VIEW', APP . '/front');
 define('DIR_BACK', APP . '/back');
+define('DB_NAME', 'u3654617-suu');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_HOST', 'localhost');
 
 $route = [
     '/',
