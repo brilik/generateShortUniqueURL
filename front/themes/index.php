daa8ac4a514d0a7ec3fb1980f8c3b77e04010447
brilik <megabrilik@gmail.com>
2020-03-16 01:19:44 +0200

    git add meta and links to sign..

--- front/themes/index.php
+++ front/themes/index.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="title">
+    <meta name="description">
     <title>Get Short Unique URL</title>
 </head>
 <body>
@@ -18,6 +20,13 @@
             <nav class="navbar navbar-default">
                 <ul class="nav navbar-nav">
                     <li><a href="/">Home</a></li>
+                    <li>
+                        <?php if(is_user_logged_in()):  ?>
+                        <a href="/admin">Dashboard</a>
+                        <?php else: ?>
+                        <a href="/signin">Login\Registration</a>
+                        <?php endif; ?>
+                    </li>
                 </ul>
             </nav>
         </div>
