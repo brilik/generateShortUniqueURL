e99530fc07f31be1844ead0f1997016a403090a9
brilik <megabrilik@gmail.com>
2020-03-16 01:13:17 +0200

    Fixed func include for admin and theme

--- back/functions.php
+++ back/functions.php
@@ -7,8 +7,12 @@ if (!function_exists('the_block')) {
             if ($pathToFile == '') {
                 throw new Exception('Path to file is empty.');
             }
-            $pathToFile = str_replace('%__%', '/', $pathToFile);
-            $pathToFile = DIR_VIEW . "/themes/{$pathToFile}.php";
+            $pathToFile = str_replace('__', '/', $pathToFile);
+            $front = '';
+            if (strstr($pathToFile, 'adminka') === false) {
+                $front = 'themes/';
+            }
+            $pathToFile = DIR_VIEW . "/{$front}{$pathToFile}.php";
             if (!file_exists($pathToFile)) {
                 throw new Exception('Path to file not correctly or file not exists.');
             }
@@ -22,7 +26,18 @@ if (!function_exists('get_template_directory_uri')) {
     function get_template_directory_uri()
     {
         $path = str_replace(APP, '', DIR_VIEW);
-
         return $_SERVER['HTTP_ORIGIN'] . $path . '/themes';
+    }
+}
+if(!function_exists('admin_url')){
+    function admin_url(){
+        $path = str_replace(APP, '', DIR_ADMIN);
+        return $_SERVER['HTTP_ORIGIN'] . $path;
+    }
+}
+if(!function_exists('is_user_logged_in')) {
+    function is_user_logged_in()
+    {
+        return isset($_COOKIE['user_logged']) ?? false;
     }
 }
