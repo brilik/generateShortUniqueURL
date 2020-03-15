43ca17c975f9237f994707133f76d6a94da90d58
brilik <megabrilik@gmail.com>
2020-03-16 01:18:08 +0200

    add routes

--- options.php
+++ options.php
@@ -2,13 +2,19 @@
 
 define('APP', dirname(__FILE__));
 define('DIR_VIEW', APP . '/front');
+define('DIR_ADMIN', DIR_VIEW . '/adminka');
 define('DIR_BACK', APP . '/back');
 define('DB_NAME', 'u3654617-suu');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_HOST', 'localhost');
 
-$route = [
+$routes = [
     '/',
-    'admin',
+    '/admin',
+    '/admin/add',
+    '/admin/edit',
+    '/admin/remove',
+    '/signin',
+    '/signup',
 ];
