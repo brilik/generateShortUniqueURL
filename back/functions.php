2d962c169f3d78054ef96ab9305feab6ae1a1737
brilik <megabrilik@gmail.com>
2020-03-15 05:25:45 +0200

    Added: exists functions

--- back/functions.php
+++ back/functions.php
@@ -1,28 +1,28 @@
 <?php
+if (!function_exists('the_block')) {
+    function the_block(string $pathToFile, $args = [])
+    {
+        try {
 
-function the_block( string $pathToFile, $args = []){
-    try{
-
-        if( $pathToFile == '' ){
-            throw new Exception('Path to file is empty.');
+            if ($pathToFile == '') {
+                throw new Exception('Path to file is empty.');
+            }
+            $pathToFile = str_replace('%__%', '/', $pathToFile);
+            $pathToFile = DIR_VIEW . "/themes/{$pathToFile}.php";
+            if (!file_exists($pathToFile)) {
+                throw new Exception('Path to file not correctly or file not exists.');
+            }
+            require $pathToFile;
+        } catch (Exception $e) {
+            echo $e->getMessage();
         }
-
-        $pathToFile = str_replace('%__%', '/', $pathToFile);
-
-        $pathToFile = DIR_VIEW . "/themes/{$pathToFile}.php";
-
-        if( ! file_exists($pathToFile) ){
-            throw new Exception('Path to file not correctly or file not exists.');
-        }
-
-        require $pathToFile;
-
-    } catch ( Exception $e){
-        echo $e->getMessage();
     }
 }
+if (!function_exists('get_template_directory_uri')) {
+    function get_template_directory_uri()
+    {
+        $path = str_replace(APP, '', DIR_VIEW);
 
-function get_template_directory_uri(){
-    $path = str_replace(APP, '', DIR_VIEW);
-    return $_SERVER['HTTP_ORIGIN'] . $path . '/themes';
+        return $_SERVER['HTTP_ORIGIN'] . $path . '/themes';
+    }
 }
