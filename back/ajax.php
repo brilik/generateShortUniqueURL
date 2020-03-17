50f93e5744f98be8551cdbe1081dd5d64f3517ea
brilik <megabrilik@gmail.com>
2020-03-17 16:46:10 +0200

    Added: processing request

--- back/ajax.php
+++ back/ajax.php
@@ -1,21 +1,68 @@
 <?php
-if (!isset($_POST['url']) || empty($_POST['url'])) {
-    die(json_encode(['result' => 'Your need enter URL']));
-}
-
 require_once '../options.php';
 require_once DIR_BACK . '/class/Debug.php';
 require_once DIR_BACK . '/class/DataBase.php';
 require_once DIR_BACK . '/class/Url.php';
+require_once DIR_BACK .'/functions.php';
 
-$res = '';
-$__post = array_map('htmlspecialchars', $_POST);
+$_POST = json_decode($_POST['obj']);
 $db = new DataBase(DB_NAME, DB_HOST, DB_USER, DB_PASS);
 $url = new Url();
 
+if ($_POST->action == 'singup') {
+    $output['res'] = $_POST->action;
+    if($existLogin = is_exist_login($db, $_POST->login)){
+        $output['res'] = 'error';
+        $output['exist'] = $existLogin;
+    }
+    if ($login = is_login_true($_POST->login)) {
+        $output['res'] = 'error';
+        $output['login'] = $login;
+    }
+    if ($password = is_pass_match($_POST->password, $_POST->repassword)) {
+        $output['res'] = 'error';
+        $output['password'] = $password;
+    }
+    if ($output['res'] === $_POST->action) {
+        setcookie("login", $_POST->login, time()+3600, "/","", 0);
+        $db->query("INSERT INTO users (login, password) VALUE ('{$_POST->login}','{$_POST->password}')");
+    }
+
+    die(json_encode($output));
+}
+if ($_POST->action == 'singin') {
+    $output['res'] = $_POST->action;
+    if($existLogin = is_exist_login($db, $_POST->login, true)){
+        $output['res'] = 'error';
+        $output['exist'] = $existLogin;
+    }
+    if ($password = pass_apply_login($_POST->login, $_POST->password, $db)) {
+        $output['res'] = 'error';
+        $output['password'] = $password;
+    }
+    if($output['res'] === $_POST->action){
+        setcookie("signin", true, time()+3600, "/","", 0);
+    }
+    die(json_encode($output));
+}
+
+
+// ********************* GOOOOD
+//die(json_encode(['error'=>'END']));
+if (!isset($_POST->url) || empty($_POST->url)) {
+    die(json_encode(['result' => 'Your need enter URL']));
+}
+
+
+/// *********************************************************
+
+
+$res = '';
+$__post = array_map('htmlspecialchars', $_POST);
+
 // Check existence URL in database
 if ($res = $db->is_existence($__post['url'], 'url')) {
-    exit(json_encode($_SERVER['HTTP_ORIGIN'] . '/' . $res['token']));
+    die (json_encode($_SERVER['HTTP_ORIGIN']. '/' . $res[0]['token']));
 }
 
 // create token
@@ -34,4 +81,3 @@ $db->close();
 
 // Show in front
 exit(json_encode($_SERVER['HTTP_ORIGIN'] . '/' . $__post['token']));
-
